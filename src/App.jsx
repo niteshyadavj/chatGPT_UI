0f840de41fa3cac512632bc74416b8023c88dcfd
niteshyadavj <ynitesh393@gmail.com>
2024-04-19 19:53:40 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+import { Cta,Brand,Navbar } from './components'
+import { Blog,Features,Footer,Header,WhatGPT3,Possibility} from './containers';
+
+
+function App() {
+  
+
+  return (
+    <div className='App'>
+     <div className="gradient__bg">
+      <Navbar/>
+      <Header/>
+     </div>
+     <Brand/>
+     <WhatGPT3/>
+     <Features/>
+     <Possibility/>
+     <Cta/>
+     <Blog/>
+     <Footer/>
+        
+    </div>
+  )
+}
+
+export default App
